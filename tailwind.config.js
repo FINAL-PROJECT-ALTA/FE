57bf6f12b1100623a8221cba0f9c86aae65ff9ea
dickyrex19 <enjoybeat19@gmail.com>
2022-03-05 16:16:38 +0700

    add profile layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,23 +6,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'white': '#ffffff',
-        'midnight': '#121063',
-        'metal': '#565584',
-        'tahiti': '#3ab7bf',
-        'silver': '#ecebff',
-        'bubble-gum': '#ff77e9',
-        'floor': '#EFF8F7',
-        'dark-green': '#013542',
-        'light-green': '#E9F9B0',
-        'light-orange': '#FFE598',
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        midnight: "#121063",
+        metal: "#565584",
+        tahiti: "#3ab7bf",
+        silver: "#ecebff",
+        "bubble-gum": "#ff77e9",
+        "mexican-pink": "#fa1d58",
+        floor: "#EFF8F7",
+        "dark-green": "#013542",
+        "light-green": "#E9F9B0",
+        "light-orange": "#FFE598",
       },
       fontFamily: {
-        poppins: ['Inter', 'sans-serif'],
+        poppins: ["Inter", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
