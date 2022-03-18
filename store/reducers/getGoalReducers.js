c951a371b1e490a7234d2b8ba14174ec23aa0875
unknown <muhamadfahmi.f@gmail.com>
2022-03-18 20:45:08 +0700

    revisi goal api

--- store/reducers/getGoalReducers.js
+++ store/reducers/getGoalReducers.js
@@ -2,7 +2,7 @@ const initialState = []
 
 const getGoalReducers = (state = initialState, action) => {
     if (action.type === "SET_USER") {
-        if (Array.isArray(action.payload)) return action.payload
+        return action.payload
     }
 
     return state
